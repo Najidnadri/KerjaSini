a22fefcb6d41c0c34821b6affa9a404901048348
Najidnadri <muhd.najid.nadri@gmail.com>
2022-06-02 23:26:43 +0800

    added a bit of struct

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,37 @@
+struct LoginDetails {
+    creds: Creds,
+    pass: String,
+}
+
+enum Creds {
+    Email,
+    Name,
+    PhoneNumber,
+}
+
+struct SignupDetails {
+    name: String,
+    email: String,
+    phone_number: String,
+    password: String,
+    postcode: String,
+}
+
+impl SignupDetails {
+    fn new() -> Self {
+        SignupDetails { name: String::new(), email: String::new(), phone_number: String::new(), password: String::new(), postcode: String::new() }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    //set up demo data
+    let signup_demo = SignupDetails{
+        name: "Najidnadri".to_string(),
+        email: "muhd.najid.nadri@gmail.com".to_string(),
+        phone_number: "0193187167".to_string(),
+        password: "12345678".to_string(),
+        postcode: "143143".to_string(),
+    };
+
+    
 }
